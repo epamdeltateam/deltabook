3f10a35f0ee0936a282cc13245332bd32f290fe8
Roman <romabukhtiarov@gmail.com>
2019-02-14 14:12:41 +0300

    Display in userpanel name and surname

--- src/main/java/com/deltabook/controller/UserController.java
+++ src/main/java/com/deltabook/controller/UserController.java
@@ -66,6 +66,8 @@ public class UserController {
         image_string = Base64.getEncoder().encodeToString (currentUser.getPicture());
 
         modelAndView.addObject("image", image_string);
+        modelAndView.addObject("name", currentUser.getFirstName());
+        modelAndView.addObject("surname", currentUser.getLastName());
         modelAndView.setViewName("user_panel");
         return modelAndView;
     }
