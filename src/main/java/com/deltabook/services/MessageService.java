89de20fee4ab3b38bc9b752c2f53bf0bd9bc52fb
Roman <romabukhtiarov@gmail.com>
2019-02-16 12:20:06 +0300

    Start implementing reading messages

--- src/main/java/com/deltabook/services/MessageService.java
+++ src/main/java/com/deltabook/services/MessageService.java
@@ -4,8 +4,12 @@ import com.deltabook.model.Message;
 import com.deltabook.model.User;
 import com.deltabook.model.send.SendMessage;
 
+import java.util.List;
+
 public interface MessageService {
 
     Message sendMessage(User userFrom, SendMessage sendMessage);
+
+    Message findByRecipientIDOrderByCreatedAt(User recipientId);
 
 }
