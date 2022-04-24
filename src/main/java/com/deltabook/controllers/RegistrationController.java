d65c87dee3077a4a429040bc5a7d440418c82abc
Roman <romabukhtiarov@gmail.com>
2022-04-24 16:11:41 +0300

    Validate user data in Registration Controller

--- src/main/java/com/deltabook/controllers/RegistrationController.java
+++ src/main/java/com/deltabook/controllers/RegistrationController.java
@@ -10,28 +10,46 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class RegistrationController {
 
-    private final int WEAK_STRENTH = 1;
-    private final int FEAR_STRENGTH = 5;
-    private final int STRONG_STRENGTH = 7;
-
     @Autowired
     private UserService userService;
 
     @GetMapping("/registration")
-    public String getRegistrationPage(Model model) {
+    private String getRegistrationPage(Model model) {
         model.addAttribute("objectToFill", new User());
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String registerUser(@ModelAttribute User insertedObject) {
-        userService.registerUser(insertedObject);
-        return "login";
+    private String registerUser(Model model, @ModelAttribute User insertedObject) {
+        StringBuilder errorSb = new StringBuilder();
+        if (insertedObject.getLogin().isEmpty()) {
+            errorSb.append("Поле с никнеймом не может быть пустым. <br>");
+        }
+        if (insertedObject.getFirstName().isEmpty()) {
+            errorSb.append("Поле с именем не может быть пустым. <br>");
+        }
+        if (insertedObject.getLastName().isEmpty()) {
+            errorSb.append("Поле с фамилией не может быть пустым. <br>");
+        }
+        if (insertedObject.getPassword().isEmpty()) {
+            errorSb.append("Поле с паролем не может быть пустым.");
+        }
+        if (errorSb.length() == 0) {
+            userService.registerUser(insertedObject);
+            return "login";
+        } else {
+            model.addAttribute("errorText", errorSb.toString());
+            return "error_register";
+        }
     }
 
     @GetMapping(value = "/checkStrength", produces = {"text/html; charset-UTF-8"})
-    public @ResponseBody
+    private @ResponseBody
     String checkPassword(@RequestParam String password) {
+        final int WEAK_STRENTH = 1;
+        final int FEAR_STRENGTH = 5;
+        final int STRONG_STRENGTH = 7;
+
         if (password.length() >= WEAK_STRENTH & password.length() < FEAR_STRENGTH) {
             return "слабый";
         } else if (password.length() >= FEAR_STRENGTH & password.length() < STRONG_STRENGTH) {
