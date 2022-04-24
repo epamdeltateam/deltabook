114a26d6d5afe8a6bb6dd5c989e15a7abc3ce8b9
Roman <romabukhtiarov@gmail.com>
2022-04-24 15:08:53 +0300

    Validate user data in Admin Controller

--- src/main/java/com/deltabook/controllers/AdminController.java
+++ src/main/java/com/deltabook/controllers/AdminController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
     @Autowired
     private UserService userService;
+    private final static String USER_CAN_NOT_DELETE_HIMSELF_ERROR = "Пользователь не может удалить сам себя.";
 
     @RequestMapping("/main_admin")
     public String mainAdmin(Authentication authentication, Model model) {
@@ -23,32 +24,41 @@ public class AdminController {
     }
 
     @RequestMapping("/change_user_last_name")
-    public String changeUserLastName(Authentication authentication, Model model, @ModelAttribute SendChangeUser SendChangeUser) {
-        userService.changeLastNameUser(SendChangeUser);
-        model.addAttribute("SendChangeUser", new SendChangeUser());
-        return "main_admin";
+    public String changeUserLastName(Model model, @ModelAttribute SendChangeUser sendChangeUser) {
+        String newLastName = sendChangeUser.getNewLastName();
+        if(newLastName != null && !newLastName.isEmpty()) {
+            userService.changeLastNameUser(sendChangeUser);
+            model.addAttribute("SendChangeUser", new SendChangeUser());
+            return "main_admin";
+        }
+        else {
+            model.addAttribute("errorText", "Новая фамилия не может быть пустой строкой.");
+            return "error";
+        }
     }
 
     @RequestMapping("/delete_user_temp")
-    public String deleteUserTemp(Authentication authentication, Model model, @ModelAttribute SendChangeUser SendChangeUser) {
+    public String deleteUserTemp(Authentication authentication, Model model, @ModelAttribute SendChangeUser sendChangeUser) {
         model.addAttribute("SendChangeUser", new SendChangeUser());
         User currentUser = ((UserDetailsImpl) authentication.getPrincipal()).getUser();
-        if (!SendChangeUser.getNickName().equals(currentUser.getLogin())) {
-            userService.deleteUserTemp(SendChangeUser);
+        if (!sendChangeUser.getNickName().equals(currentUser.getLogin())) {
+            userService.deleteUserTemp(sendChangeUser);
             return "main_admin";
         } else {
+            model.addAttribute("errorText", USER_CAN_NOT_DELETE_HIMSELF_ERROR);
             return "error";
         }
     }
 
     @RequestMapping("/delete_user_total")
-    public String deleteUserTotal(Authentication authentication, Model model, @ModelAttribute SendChangeUser SendChangeUser) {
+    public String deleteUserTotal(Authentication authentication, Model model, @ModelAttribute SendChangeUser sendChangeUser) {
         model.addAttribute("SendChangeUser", new SendChangeUser());
         User currentUser = ((UserDetailsImpl) authentication.getPrincipal()).getUser();
-        if (!SendChangeUser.getNickName().equals(currentUser.getLogin())) {
-            userService.deleteUserTotal(SendChangeUser);
+        if (!sendChangeUser.getNickName().equals(currentUser.getLogin())) {
+            userService.deleteUserTotal(sendChangeUser);
             return "main_admin";
         } else {
+            model.addAttribute("errorText", USER_CAN_NOT_DELETE_HIMSELF_ERROR);
             return "error";
         }
     }
